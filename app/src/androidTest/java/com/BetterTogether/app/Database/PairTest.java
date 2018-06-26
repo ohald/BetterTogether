69747611474f2e943f2698f2644183628d136226
esog <esog@statoil.com>
2018-06-26 12:16:57 +0200

    add tests for database

---
+++ app/src/androidTest/java/com/BetterTogether/app/Database/PairTest.java
@@ -0,0 +1,78 @@
+package com.BetterTogether.app.Database;
+
+import android.arch.persistence.room.Room;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import DB.Dao.PairDao;
+import DB.Dao.PersonDao;
+import DB.SQLiteDB;
+import DB.Tables.Pair;
+import DB.Tables.Person;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThat;
+
+public class PairTest {
+
+    private PairDao mPairDao;
+    private PersonDao mPersonDao;
+    private SQLiteDB mDb;
+    private Pair pair;
+    private GregorianCalendar calendar = new GregorianCalendar(1900, 01,1,00,00,00);
+    private Date date = new Date(calendar.getTimeInMillis());
+
+    @Before
+    public void createDb() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        mDb = Room.inMemoryDatabaseBuilder(context, SQLiteDB.class).build();
+        mPairDao = mDb.pairDao();
+        mPersonDao = mDb.personDao();
+    }
+
+    @After
+    public void closeDb(){
+        mDb.close();
+    }
+
+    private void addPairToDB(){
+        Person esog = new Person("esog");
+        Person ohald = new Person("ohald");
+        mPersonDao.insertPerson(esog);
+        mPersonDao.insertPerson(ohald);
+
+        this.pair = new Pair(new Date());
+        pair.setPerson1("esog");
+        pair.setPerson2("ohald");
+        mPairDao.insertPair(pair);
+    }
+
+
+    @Test
+    public void addPairToEmptyDBGivesOnePairInDB(){
+        addPairToDB();
+        assertThat(1, equalTo(mPairDao.getHistory(date).size()));
+    }
+
+    @Test
+    public void deletePairFromDBRemovesPairFromDB(){
+        addPairToDB();
+        mPairDao.deletePair(pair);
+
+       assertThat(mPairDao.getTimesPairProgrammed("ohald"), equalTo(0));
+       assertThat(mPairDao.getTimesPairProgrammed("esog"), equalTo(0));
+       assertThat(mPairDao.getPairProgrammingTotalFromDate(date), equalTo(0));
+    }
+
+
+}
