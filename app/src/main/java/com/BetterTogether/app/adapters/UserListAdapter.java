3ee3124adad37997098322c4858e9bf933160e99
esog <esog@statoil.com>
2018-07-31 14:59:09 +0200

    Remove ImageReader class
    Only one useful method. Move method to UserListAdapter.

--- app/src/main/java/com/BetterTogether/app/adapters/UserListAdapter.java
+++ app/src/main/java/com/BetterTogether/app/adapters/UserListAdapter.java
@@ -2,6 +2,9 @@ package com.BetterTogether.app.adapters;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,16 +15,17 @@ import com.BetterTogether.app.R;
 import java.util.List;
 
 import com.BetterTogether.app.Person;
-import DB.ImageReader;
 
 public class UserListAdapter extends BaseAdapter {
 
     private LayoutInflater inflater;
     private List<Person> dataSet;
+    private Drawable defaultImage;
 
     public UserListAdapter(Context context, List<Person> dataSet) {
         this.dataSet = dataSet;
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        defaultImage = inflater.getContext().getDrawable(R.drawable.unknown);
     }
 
     @Override
@@ -48,27 +52,35 @@ public class UserListAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View view, ViewGroup parent) {
         View cellView = inflater.inflate(R.layout.user_list_element, parent, false);
-        //set images to corresponding users
         String image = dataSet.get(position).getImage();
         ImageView imageView = cellView.findViewById(R.id.profile_image);
-        Bitmap bitmap;
 
 
+        //user has no image
         if(image.equalsIgnoreCase("unknown")){
-            bitmap = ImageReader.imageToBitmap(cellView.getContext(), "unknown");
+            imageView.setImageDrawable(defaultImage);
         } else {
-            bitmap = ImageReader.base64ToScaledBitmap(image);
+            imageView.setImageBitmap(base64ToScaledBitmap(image));
         }
-        imageView.setImageBitmap(bitmap);
 
 
         TextView name = cellView.findViewById(R.id.username);
 
-        //display first name
-        String displayedText = dataSet.get(position).getName();
-        String[] parts = displayedText.split(" ");
-        name.setText(parts[0]);
+        name.setText(getFirstName(dataSet.get(position).getName()));
         return cellView;
+    }
+
+    private String getFirstName(String name){
+        String[] parts = name.split(" ");
+        return parts[0];
+    }
+
+    private Bitmap base64ToScaledBitmap(String decodedImage){
+        byte [] image = Base64.decode(decodedImage, Base64.DEFAULT);
+        Bitmap b = BitmapFactory.decodeByteArray(image, 0, image.length);
+        Bitmap scaled = Bitmap.createScaledBitmap(b, 100, 100, false);
+        b.recycle();
+        return scaled;
     }
 
 }
